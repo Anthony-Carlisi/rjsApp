24b54fc307e8aebd1a46f52e56a78adbca90963d
Anthony Carlisi <anthonycarlisi95@gmail.com>
2022-05-15 09:05:28 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  // Route,
+  // Routes,
+  //  NavLink,
+} from 'react-router-dom'
+import SmsChatWindow from './components/SmsChatWindow'
+import Test from './components/Test'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Test />
+    </Router>
+  )
 }
 
-export default App;
+export default App
